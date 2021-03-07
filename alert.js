b7ad2f1240b70e738d351c33cf267853e9de238d
Владислав <56509864+Vladislav17007@users.noreply.github.com>
2021-03-07 23:26:54 +0500

    Добавление новых проектов в мой репозиторий
    стили, скрипты и веб-сайт

---
+++ alert.js
@@ -0,0 +1,21 @@
+"use strict"
+$('document').
+ready(function(){
+    $('body').append('<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjj97j4jPrsAhVJAxAIHULHD3IQFjAAegQIARAD&url=https%3A%2F%2Fwww.anilibria.tv%2F&usg=AOvVaw1sasaQIaJWfChS3HWKNix4">Аниме с лучшей озвучкой и наивысшим качеством, только на АниЛибрия!!! </a>')
+
+    $('footer').remove('');
+
+});
+
+let HI = 'Hello world!';
+
+{
+    console.log('HI');
+    console.log
+}
+
+alert('HI');
+
+function square(number) {
+    return number * number;
+}
